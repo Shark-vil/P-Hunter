d0b1a4b040aae8c0daf3ed7ca460c439d43bc57d
Shark_vil <sharkvil228@gmail.com>
2020-05-19 03:20:49 +0300

    Temporarily remove custom connection

--- Assets/Project/Scripts/Network/NetworkManagerPoniebHunter.cs
+++ Assets/Project/Scripts/Network/NetworkManagerPoniebHunter.cs
@@ -7,36 +7,5 @@ using UnityEngine;
 
 public class NetworkManagerPoniebHunter : NetworkManager
 {
-    public List<PlayerTypeObjects> PlayerPrefabs = new List<PlayerTypeObjects>();
-    public List<string> PlayersTypeExists = new List<string>();
 
-    public override void OnStartServer()
-    {
-        base.OnStartServer();
-
-        System.Random rnd = new System.Random();
-        PlayerPrefabs = PlayerPrefabs.OrderBy(x => rnd.Next()).ToList();
-    }
-
-    public override void OnServerConnect(NetworkConnection conn)
-    {
-        base.OnServerConnect(conn);
-
-        PlayerTypeObjects PlayerNewPrefab = null;
-        for (int i = 0; i < PlayerPrefabs.Count; i++)
-        {
-            PlayerTypeObjects PlayerPrefab = PlayerPrefabs[i];
-            if (!PlayersTypeExists.Exists(x => x == PlayerPrefab.character))
-            {
-                PlayersTypeExists.Add(PlayerPrefab.character);
-                PlayerNewPrefab = PlayerPrefab;
-                break;
-            }
-        }
-
-        if (PlayerNewPrefab == null)
-            conn.Disconnect();
-
-        NetworkServer.AddPlayerForConnection(conn, Instantiate(PlayerNewPrefab.playerObject));
-    }
 }

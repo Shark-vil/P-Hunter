f346ffa3e508d0eb838d30d3af651288cccfee2e
Shark_vil <sharkvil228@gmail.com>
2020-05-19 03:04:09 +0300

    Testing custom multi-character connects

--- Assets/Project/Scripts/Network/NetworkManagerPoniebHunter.cs
+++ Assets/Project/Scripts/Network/NetworkManagerPoniebHunter.cs
@@ -1,9 +1,42 @@
 ﻿using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class NetworkManagerPoniebHunter : NetworkManager
 {
+    public List<PlayerTypeObjects> PlayerPrefabs = new List<PlayerTypeObjects>();
+    public List<string> PlayersTypeExists = new List<string>();
 
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        System.Random rnd = new System.Random();
+        PlayerPrefabs = PlayerPrefabs.OrderBy(x => rnd.Next()).ToList();
+    }
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        base.OnServerConnect(conn);
+
+        PlayerTypeObjects PlayerNewPrefab = null;
+        for (int i = 0; i < PlayerPrefabs.Count; i++)
+        {
+            PlayerTypeObjects PlayerPrefab = PlayerPrefabs[i];
+            if (!PlayersTypeExists.Exists(x => x == PlayerPrefab.character))
+            {
+                PlayersTypeExists.Add(PlayerPrefab.character);
+                PlayerNewPrefab = PlayerPrefab;
+                break;
+            }
+        }
+
+        if (PlayerNewPrefab == null)
+            conn.Disconnect();
+
+        NetworkServer.AddPlayerForConnection(conn, Instantiate(PlayerNewPrefab.playerObject));
+    }
 }
---
+++ Assets/Project/Scripts/Player/PlayerType.cs
@@ -0,0 +1,23 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class PlayerTypeMessage : MessageBase
+{
+    public string name;
+}
+
+[System.Serializable]
+public class PlayerTypeObjects
+{
+    public string character;
+    public GameObject playerObject;
+
+    public PlayerTypeObjects(string character, GameObject playerObject)
+    {
+        this.character = character;
+        this.playerObject = playerObject;
+    }
+}
